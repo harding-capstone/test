1cbc8d2c5c274b36ba82a23f4bb3edad9318a202
ajarrett29 <austin.jarrett29@gmail.com>
2019-01-26 19:59:28 -0600

    commit

--- src/main/java/com/shepherdjerred/capstone/test/Main.java
+++ src/main/java/com/shepherdjerred/capstone/test/Main.java
@@ -3,5 +3,6 @@ package com.shepherdjerred.capstone.test;
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello, world!");
+    new AustinJarrettClass().newString("This is a string");
   }
 }
